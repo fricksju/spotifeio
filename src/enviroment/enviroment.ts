f96285dc9832b5d1d12573435b7635827aa9578c
fricksju <jfrickssoares@gmail.com>
2025-08-19 20:03:51 -0300

    alteracoes

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,3 +1,20 @@
-export const environment = {
+export const environment = { //constante visivel sistema interiro 
   production: false
+}
+
+export const SportifyConfiguration = {
+  clientId: '985ca65f21ec48a8bdf9449f2d3e31f8',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  apiTokenEdpoint: 'https://accounts.spotify.com/api/token',
+  redirectUrl: 'http://127.0.0.1/login',
+  scopes: [
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-top-read',
+    'user-modify-playback-state',
+    'user-library-read',
+    'playlist-read-private',
+    'playlist-read-collaborative'
+  ]
 }
